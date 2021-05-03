8d568093ac38021525de369e4c467aa5fff2fdc0
zahrakazemii <79324179+zahrakazemii@users.noreply.github.com>
2021-05-03 18:27:55 +0430

    Update swap.py

--- ex1/swap.py
+++ ex1/swap.py
@@ -3,6 +3,6 @@ def swap(s1, s2):
     return s2, s1
 
 s1 = input("please enter firt number:")
-s2 = input("please enter second number:)
+s2 = input("please enter second number:")
 s1, s2 = swap(s1, s2)
 print (s1, s2 )
